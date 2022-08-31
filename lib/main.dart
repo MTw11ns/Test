a8d750b8d0ef58392b0f134117ad8362233d6b47
GomaaFluteer <mohamedgomaa5311@gmail.com>
2022-08-31 15:17:38 +0200

    user two

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'user one',
+      title: 'Flutter test github1111',
       theme: ThemeData(
         // This is the theme of your application.
         //
